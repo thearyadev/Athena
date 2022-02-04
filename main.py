4ddbe3542b2b9034b6a68fa6e19cbed06b1bee50
thearyadev <87589047+thearyadev@users.noreply.github.com>
2022-02-04 00:01:44 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from utils.tools import Athena
 
 server = Athena()
 server.configs.rich_presence = "MAINTENANCE MODE"
-server.configs.token = "OTIzMzU3NDU3NTU0NzQzMzI4.YcO1pQ.HHFd6ma5FHJnrqSP38yy6_OMxzg"
-server.configs.testing_token = "ODQyODc2ODEyMTIwMTYyMzc0.YJ7sMw.Hag483Jk0QTHp-k8IF5nU8Dhu34"
+server.configs.token = ""
+server.configs.testing_token = ""
+server.configs.refresh()
 server.initialize(mode="-t")
