172429a1314119f0c3065e8dbfdc0a3ed7fd02a8
thearyadev <87589047+thearyadev@users.noreply.github.com>
2022-02-14 11:11:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from utils.tools import Athena
 def main():
     server = Athena()
     server.configs.token = "YOUR TOKEN HERE"
-    server.configs.version = "4.3.0"
-    server.initialize(mode="-d")
+    server.configs.version = "4.3.3"
+    server.initialize(mode=Athena.DISTRIBUTION)
 
 
 if __name__ == '__main__':
