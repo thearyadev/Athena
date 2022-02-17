644c5b27fdf3cac1950b6589d4348af4a636b985
Arya <personal@aryankothari.dev>
2022-02-17 13:32:31 -0500

    update to main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from utils.tools import Athena
 
 def main():
     server = Athena()
-    server.configs.token = "ODQyODc2ODEyMTIwMTYyMzc0.YJ7sMw.CRY5zHxqfWNGPneWLQw1alQQ1Qw"
+    server.configs.token = "YOUR TOKEN HERE"
     server.initialize(mode=Athena.DISTRIBUTION)
 
 
