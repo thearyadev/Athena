ef2dfcbdb81f5a6f64c1af86a3c16d0b05759e8f
Arya <personal@aryankothari.dev>
2022-02-17 13:12:02 -0500

    bug fixes for Urban Dictionary lookup

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from utils.tools import Athena
 def main():
     server = Athena()
     server.configs.token = ""
-    server.configs.testing_token = "ODQyODc2ODEyMTIwMTYyMzc0.YJ7sMw.CRY5zHxqfWNGPneWLQw1alQQ1Qw"
+    server.configs.testing_token = "poggers"
     server.configs.version = "4.4.0"
     server.initialize(mode=Athena.TESTING)
 
