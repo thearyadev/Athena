74fc5ac7360e8482f943e8ab7019ee09dc048216
Arya <personal@aryankothari.dev>
2022-03-10 11:49:40 -0500

    Add team section, role select boilerplate

--- utils/commands/__init__.py
+++ utils/commands/__init__.py
@@ -4,9 +4,10 @@ from .Events import events
 from .General import general
 from .Moderation import moderation
 from .PUGs import pugs
-from .RepairChannelOrder import repair_channel_order
 from .RSVP import rsvp
 from .AdminConfigs import admin_configs
 from .DatabaseModifiers import database_modifiers
 from .Backup import backup
 from .UrbanDictionary import urban_dictionary
+from .TeamBuilder import team_builder
+from .RoleSelect import role_select
